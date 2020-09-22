8d1f569dcb8d363494b7ec589d03d8581db40176
jakersmith <71725421+jakersmith@users.noreply.github.com>
2020-09-22 13:57:51 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+# Jacob Smith
+# A01476417
+# Assn15-task1
+# This program runs the Blackjack game
+
+from src.blackjack import Blackjack
+
+
+def main():
+    game = Blackjack()
+    game.playGame()
+
+
+main()
